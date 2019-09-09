132663ad0fca170a2e5854a3f8890d1f3c46e12a
Rony <rony.aguilar.dev@gmail.com>
2019-09-09 14:28:08 -0700

    refactored config for heroku

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+    "MONGO_URI": process.env.MONGO_URI,
+    "cookieKey": process.env.cookieKey,
+    "GOOGLE_CLIENT_SECRET": process.env.GOOGLE_CLIENT_SECRET,
+    "GOOGLE_CLIENT_ID": process.env.GOOGLE_CLIENT_ID
+  }
